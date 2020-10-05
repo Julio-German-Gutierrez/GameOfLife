5ef57e1bf9851a0f4b776fb70a264865df95a79d
Julio-German-Gutierrez <jgermangutierrez@gmail.com>
2020-10-05 18:18:23 +0200

    Cambios

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
@@ -9,10 +10,10 @@ namespace GameOfLife
         static bool[,] world = new bool[40, 150];
         static bool[,] newWorld = new bool[40, 150];
 
-        static int count = 0;
-
         static void Main(string[] args)
         {
+
+
             for (int y = 0; y < world.GetLength(0); y++)
             {
                 for (int x = 0; x < world.GetLength(1); x++)
@@ -22,8 +23,10 @@ namespace GameOfLife
                 }
             }
 
-            //Pattern(68, 13);
-            LoadPattern(world, "/home/lalegion/DotNET/Lektioner/GameOfLife/Pattern 2.txt");
+            string fileName = SelectPattern();
+
+            string path = "./";
+            LoadPattern(world, path + "Pattern 2.gol");
 
             Print(world, true);
             Console.Write("Press any key to START...");
@@ -32,7 +35,22 @@ namespace GameOfLife
             Timer timer = new Timer(NextStep, new AutoResetEvent(false), 1000, 250);
 
             Console.ReadKey();
+            Console.ResetColor();
+            Console.Clear();
+        }
 
+        static string SelectPattern()
+        {
+            var files = System.IO.Directory.EnumerateFiles("./");
+            foreach (string f in files)
+            {
+                //if (f.Contains(".gol")) System.Console.WriteLine(f);
+                System.Console.WriteLine(f);
+            }
+            System.Console.Write("\nPress any key to continue...");
+            Console.ReadKey(true);
+
+            return "Listo";
         }
 
         static void NextStep(Object stateInfo)
@@ -126,7 +144,8 @@ namespace GameOfLife
                 }
                 Console.WriteLine();
             }
-            if (!intro) System.Console.Write("Press any key to EXIT...");
+            Console.ResetColor();
+            if (!intro) System.Console.Write("Press any key to STOP de Simulation...");
         }
 
         static void LoadPattern(bool[,] w, string openFile)
@@ -188,188 +207,5 @@ namespace GameOfLife
             sw.Close();
             f.Close();
         }
-
-        /* static void Pattern(int x, int y)
-        {
-            world[y, x] = false;
-            world[y, x + 1] = false;
-            world[y, x + 2] = true;
-            world[y, x + 3] = true;
-            world[y, x + 4] = true;
-            world[y, x + 5] = false;
-
-            world[y + 1, x] = false;
-            world[y + 1, x + 1] = false;
-            world[y + 1, x + 2] = false;
-            world[y + 1, x + 3] = false;
-            world[y + 1, x + 4] = false;
-            world[y + 1, x + 5] = false;
-
-            world[y + 2, x] = true;
-            world[y + 2, x + 1] = false;
-            world[y + 2, x + 2] = false;
-            world[y + 2, x + 3] = false;
-            world[y + 2, x + 4] = false;
-            world[y + 2, x + 5] = true;
-
-            world[y + 3, x] = true;
-            world[y + 3, x + 1] = false;
-            world[y + 3, x + 2] = false;
-            world[y + 3, x + 3] = false;
-            world[y + 3, x + 4] = false;
-            world[y + 3, x + 5] = true;
-
-            world[y + 4, x] = true;
-            world[y + 4, x + 1] = false;
-            world[y + 4, x + 2] = false;
-            world[y + 4, x + 3] = false;
-            world[y + 4, x + 4] = false;
-            world[y + 4, x + 5] = true;
-
-            world[y + 5, x] = false;
-            world[y + 5, x + 1] = false;
-            world[y + 5, x + 2] = true;
-            world[y + 5, x + 3] = true;
-            world[y + 5, x + 4] = true;
-            world[y + 5, x + 5] = false;
-
-            // 1
-            /////////////////////////////////////
-            // 2
-
-            world[y, x + 7] = false;
-            world[y, x + 1 + 7] = true;
-            world[y, x + 2 + 7] = true;
-            world[y, x + 3 + 7] = true;
-            world[y, x + 4 + 7] = false;
-            world[y, x + 5 + 7] = false;
-
-            world[y + 1, x + 7] = false;
-            world[y + 1, x + 1 + 7] = false;
-            world[y + 1, x + 2 + 7] = false;
-            world[y + 1, x + 3 + 7] = false;
-            world[y + 1, x + 4 + 7] = false;
-            world[y + 1, x + 5 + 7] = false;
-
-            world[y + 2, x + 7] = true;
-            world[y + 2, x + 1 + 7] = false;
-            world[y + 2, x + 2 + 7] = false;
-            world[y + 2, x + 3 + 7] = false;
-            world[y + 2, x + 4 + 7] = false;
-            world[y + 2, x + 5 + 7] = true;
-
-            world[y + 3, x + 7] = true;
-            world[y + 3, x + 1 + 7] = false;
-            world[y + 3, x + 2 + 7] = false;
-            world[y + 3, x + 3 + 7] = false;
-            world[y + 3, x + 4 + 7] = false;
-            world[y + 3, x + 5 + 7] = true;
-
-            world[y + 4, x + 7] = true;
-            world[y + 4, x + 1 + 7] = false;
-            world[y + 4, x + 2 + 7] = false;
-            world[y + 4, x + 3 + 7] = false;
-            world[y + 4, x + 4 + 7] = false;
-            world[y + 4, x + 5 + 7] = true;
-
-            world[y + 5, x + 7] = false;
-            world[y + 5, x + 1 + 7] = true;
-            world[y + 5, x + 2 + 7] = true;
-            world[y + 5, x + 3 + 7] = true;
-            world[y + 5, x + 4 + 7] = false;
-            world[y + 5, x + 5 + 7] = false;
-
-            // 2
-            ////////////////////////////
-            // 3
-
-            world[y + 7, x] = false;
-            world[y + 7, x + 1] = false;
-            world[y + 7, x + 2] = true;
-            world[y + 7, x + 3] = true;
-            world[y + 7, x + 4] = true;
-            world[y + 7, x + 5] = false;
-
-            world[y + 1 + 7, x] = true;
-            world[y + 1 + 7, x + 1] = false;
-            world[y + 1 + 7, x + 2] = false;
-            world[y + 1 + 7, x + 3] = false;
-            world[y + 1 + 7, x + 4] = false;
-            world[y + 1 + 7, x + 5] = true;
-
-            world[y + 2 + 7, x] = true;
-            world[y + 2 + 7, x + 1] = false;
-            world[y + 2 + 7, x + 2] = false;
-            world[y + 2 + 7, x + 3] = false;
-            world[y + 2 + 7, x + 4] = false;
-            world[y + 2 + 7, x + 5] = true;
-
-            world[y + 3 + 7, x] = true;
-            world[y + 3 + 7, x + 1] = false;
-            world[y + 3 + 7, x + 2] = false;
-            world[y + 3 + 7, x + 3] = false;
-            world[y + 3 + 7, x + 4] = false;
-            world[y + 3 + 7, x + 5] = true;
-
-            world[y + 4 + 7, x] = false;
-            world[y + 4 + 7, x + 1] = false;
-            world[y + 4 + 7, x + 2] = false;
-            world[y + 4 + 7, x + 3] = false;
-            world[y + 4 + 7, x + 4] = false;
-            world[y + 4 + 7, x + 5] = false;
-
-            world[y + 5 + 7, x] = false;
-            world[y + 5 + 7, x + 1] = false;
-            world[y + 5 + 7, x + 2] = true;
-            world[y + 5 + 7, x + 3] = true;
-            world[y + 5 + 7, x + 4] = true;
-            world[y + 5 + 7, x + 5] = false;
-
-            // 3
-            ////////////////////////////////
-            // 4
-
-            world[y + 7, x + 7] = false;
-            world[y + 7, x + 1 + 7] = true;
-            world[y + 7, x + 2 + 7] = true;
-            world[y + 7, x + 3 + 7] = true;
-            world[y + 7, x + 4 + 7] = false;
-            world[y + 7, x + 5 + 7] = false;
-
-            world[y + 1 + 7, x + 7] = true;
-            world[y + 1 + 7, x + 1 + 7] = false;
-            world[y + 1 + 7, x + 2 + 7] = false;
-            world[y + 1 + 7, x + 3 + 7] = false;
-            world[y + 1 + 7, x + 4 + 7] = false;
-            world[y + 1 + 7, x + 5 + 7] = true;
-
-            world[y + 2 + 7, x + 7] = true;
-            world[y + 2 + 7, x + 1 + 7] = false;
-            world[y + 2 + 7, x + 2 + 7] = false;
-            world[y + 2 + 7, x + 3 + 7] = false;
-            world[y + 2 + 7, x + 4 + 7] = false;
-            world[y + 2 + 7, x + 5 + 7] = true;
-
-            world[y + 3 + 7, x + 7] = true;
-            world[y + 3 + 7, x + 1 + 7] = false;
-            world[y + 3 + 7, x + 2 + 7] = false;
-            world[y + 3 + 7, x + 3 + 7] = false;
-            world[y + 3 + 7, x + 4 + 7] = false;
-            world[y + 3 + 7, x + 5 + 7] = true;
-
-            world[y + 4 + 7, x + 7] = false;
-            world[y + 4 + 7, x + 1 + 7] = false;
-            world[y + 4 + 7, x + 2 + 7] = false;
-            world[y + 4 + 7, x + 3 + 7] = false;
-            world[y + 4 + 7, x + 4 + 7] = false;
-            world[y + 4 + 7, x + 5 + 7] = false;
-
-            world[y + 5 + 7, x + 7] = false;
-            world[y + 5 + 7, x + 1 + 7] = true;
-            world[y + 5 + 7, x + 2 + 7] = true;
-            world[y + 5 + 7, x + 3 + 7] = true;
-            world[y + 5 + 7, x + 4 + 7] = false;
-            world[y + 5 + 7, x + 5 + 7] = false;
-        } */
     }
 }
